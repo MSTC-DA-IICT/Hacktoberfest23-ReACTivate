aa5f48399a8d9cfda0aae2320fd8881a5b5cf6d7
Sakshi <sakshipatil0344@gmail.com>
2023-10-24 19:15:49 +0530

    Dance Club Page Created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-
 import './App.css';
-
-
-
 import Clublist from './Clublist';
 import Academic from './committees/Academic_Committee';
 import Cultural from './committees/Cultural_Committee';
+import Dance from './committees/Dance_Club';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import React from 'react'
 import Dcei from './committees/DCEI';
@@ -19,7 +16,9 @@ const App = () => {
           <Route path='/' element={<Clublist />}></Route>
           <Route path='/1' element={<Academic />}></Route>
           <Route path='/5' element={<Cultural />}></Route>
+          <Route path='/13' element={<Dance/>}></Route>
             <Route path='/14' element={<Dcei/>}></Route>
+
         </Routes>
       </BrowserRouter>
     </div>

62567d798e43e05a6d5f32d3e85d7da90b1297b2
Sakshi <sakshipatil0344@gmail.com>
2023-10-31 00:31:49 +0530

    Removed Errors

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ import Dance from './committees/Dance_Club';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import React from 'react'
 import Dcei from './committees/DCEI';
-import ClubDetails from './ClubDetails';
-import Navbar from './NavBar';
-import import Sambhav from './committees/Sambhav';
-import AI_club from './committees/AI_Club';
-
 
 const App = () => {
   return (
@@ -24,9 +19,6 @@ const App = () => {
           <Route path='/5' element={<Cultural />}></Route>
           <Route path='/13' element={<Dance/>}></Route>
             <Route path='/14' element={<Dcei/>}></Route>
-            <Route path='/:id' element={<ClubDetails />} /> {/* Dynamic route for ClubDetails */}
-            <Route path ='/19' element={<Sambhav/>}></Route>
-            <Route path='/2' element={<AI_club/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
